5b03db9e1e67c3fcadd458d0c6cdba11669a6601
Gabriel P Maia <gitgabrielmaia@gmail.com>
2025-01-26 11:20:24 -0300

    Contador de Palavras

---
+++ ContadorPalavras/ContadorPalavras/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma frase:");
+        string frase = Console.ReadLine();
+
+        string[] palavras = frase.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+        Console.WriteLine($"A frase contém {palavras.Length} palavras.");
+    }
+}
